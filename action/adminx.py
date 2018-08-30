66503b9cee688d346b6b4d8b1daba444a31ddf11
jiping-shan <jiping505@163.com>
2018-08-30 19:19:03 +0800

    添加后台修改功能 celery

--- action/adminx.py
+++ action/adminx.py
@@ -1,4 +1,11 @@
+#coding:utf-8
+from __future__ import absolute_import, unicode_literals
 from .models import hosts,group,hosts_group,task,task_result,ys_uid
+from djcelery.models import (
+  TaskState, WorkerState,
+  PeriodicTask, IntervalSchedule, CrontabSchedule,
+)
+
 import xadmin
 
 
@@ -17,6 +24,16 @@ class task_resultAdmin(object):
 class ys_uidAdmin(object):
     list_display = ['uid','host','status']
     search_fields = ['uid','status','host__ip']
+
+
+xadmin.site.register(IntervalSchedule) # 存储循环任务设置的时间
+xadmin.site.register(CrontabSchedule) # 存储定时任务设置的时间
+xadmin.site.register(PeriodicTask) # 存储任务
+xadmin.site.register(TaskState) # 存储任务执行状态
+xadmin.site.register(WorkerState) # 存储执行任务的worker
+
+
+
 xadmin.site.register(hosts, hostsAdmin)
 xadmin.site.register(group, groupAdmin)
 xadmin.site.register(hosts_group, hosts_groupAdmin)
